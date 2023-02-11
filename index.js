b5c7a3a5030e08bc468341169241677e31758c7a
md-mobarak <md-mobarak>
2023-02-12 00:34:13 +0600

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 require("dotenv").config();
 
 app.use(express.json());
